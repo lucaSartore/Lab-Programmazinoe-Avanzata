2216aea4ff0c8f6014d0da0a19fe54be709ab12e
lucaSartore <lucasar.com8@gmail.com>
2023-03-05 11:29:37 +0100

    add lab 5

---
+++ lab5/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+#include "persona.h"
+
+using namespace std;
+
+int main() {
+    Person p = Person((char*)"mario", 100);
+    Person p2 = p;
+    Person p3 = Person();
+
+
+    cout << p << endl;
+    cout << p2 << endl;
+    cout << p3 << endl;
+
+}
